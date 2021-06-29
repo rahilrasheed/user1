72054c188dc648971602ea443f9ad5b71058649a
rahilrasheed <rahilrasheed87@gmail.com>
2021-06-29 16:55:26 +0530

    t4

--- apk/urls.py
+++ apk/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
    path('user',views.fnuser),
-   path('user1',views.fnuser1)
+   path('user1',views.fnuser1),
+   path('home',views.fnhome)
 ]
--- apk/views.py
+++ apk/views.py
@@ -7,3 +7,5 @@ def fnuser(request):
     return HttpResponse("rahil are you fine")
 def fnuser1(request):
     return render(request,'user1.html')    
+def fnhome(request):
+    return render(request,'home.html')
