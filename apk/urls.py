0d250eedfc53b6dff86b03ed7bc906e3f2e16a08
rahilrasheed <rahilrasheed87@gmail.com>
2021-08-29 22:29:01 +0530

    e3

--- apk/urls.py
+++ apk/urls.py
@@ -3,5 +3,23 @@ from . import views
 urlpatterns = [
    path('user',views.fnuser),
    path('user1',views.fnuser1),
-   path('home',views.fnhome)
+   path('home',views.fnhome),
+   path('prod',views.fnprod),
+   path('Fboot',views.fnFboot),
+   path('Lboot',views.fnLboot),
+   path('NTboot2',views.fnNTboot2),
+   path('new',views.fnnew),
+   path('modjum',views.fnmod),
+   path('gridb',views.fngridb),
+   path('wb',views.fnwb),
+   path('java1',views.fnjava1),
+   path('promcon',views.fnpromcon),
+   path('arith',views.fnarith),
+   path('grade',views.fngrade),
+   path('switch1',views.fnswitch1),
+   path('fibinocci',views.fnfibi),
+   path('ques',views.fnques),
+   path('while',views.fnwhile),
+   path('hw',views.fnhw),
+   path('cyber',views.fncyber)
 ]
--- apk/views.py
+++ apk/views.py
@@ -9,3 +9,39 @@ def fnuser1(request):
     return render(request,'user1.html')    
 def fnhome(request):
     return render(request,'home.html')
+def fnprod(request):
+    return render(request,'prod.html')     
+def fnFboot(request):
+    return render(request,'Fboot.html') 
+def fnLboot(request):
+    return render(request,'Lboot.html')    
+def fnNTboot2(request):
+    return render(request,'NTboot2.html')     
+def fnnew(request):
+    return render(request,'new.html')  
+def fnmod(request):
+    return render(request,'modjum.html')   
+def fngridb(request):
+    return render(request,'gridb.html')
+def fnwb(request):
+    return render(request,'wb.html')
+def fnjava1(request):
+    return render(request,'java1.html') 
+def fnpromcon(request):
+    return render(request,'promcon.html')  
+def fnarith(request):
+    return render(request,'arith.html')   
+def fngrade(request):
+    return render(request,'grade.html')  
+def fnswitch1(request):
+    return render(request,'switch1.html')  
+def fnfibi(request):
+    return render(request,'fibinocci.html')
+def fnques(request):
+    return render(request,'ques.html')  
+def fnwhile(request):
+    return render(request,'while.html') 
+def fnhw(request):
+    return render(request,'hw.html')     
+def fncyber(request):
+    return render(request,'cyber.html')                                     
